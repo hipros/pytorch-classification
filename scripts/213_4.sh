4b5bf39da19d83d50939c3ae3a2a2a4f37fe1fc0
hipros <npclinic3@gmail.com>
2023-12-02 17:35:24 +0900

    change save_dir in scripts

--- scripts/213_4.sh
+++ scripts/213_4.sh
@@ -4,7 +4,7 @@ run() {
 	arguments="--arch $arch --depth $depth --pre-layer $pre_layer --normalization-transform $nor_option \
 		--epochs 200 --schedule 100 150 --gamma 0.1 --no-wd-pre-layer $no_wd_pre_layer \
 		--dataset $dataset \
-		--checkpoint test-$dataset-$arch$depth-pre_layer-$pre_layer-norm_trans-$nor_option \
+		--checkpoint test-$dataset-$arch$depth-pre_layer-$pre_layer-no_wd_pre_layer-$no_wd_pre_layer-norm_trans-$nor_option \
 		--gpu-id $gpu_id"
 
 	echo $arguments
