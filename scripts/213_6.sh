ebc065ce638ef7cb1b29f1f13d174d69af073b27
hipros <npclinic3@gmail.com>
2023-12-02 17:33:30 +0900

    scripts

---
+++ scripts/213_6.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+run() {
+	arguments="--arch $arch --depth $depth --pre-layer $pre_layer --normalization-transform $nor_option \
+		--epochs 200 --schedule 100 150 --gamma 0.1 --no-wd-pre-layer $no_wd_pre_layer \
+		--dataset $dataset \
+		--checkpoint test-$dataset-$arch$depth-pre_layer-$pre_layer-norm_trans-$nor_option \
+		--gpu-id $gpu_id"
+
+	echo $arguments
+
+	python3 cifar.py $arguments
+}
+
+gpu_id="6"
+nor_option="True"
+pre_layer="affine"
+no_wd_pre_layer="True"
+
+arch="resnet"
+depth="20"
+dataset="cifar10"
+run
+
+arch="resnet"
+depth="20"
+dataset="cifar100"
+run
+
+arch="vgg16_bn"
+depth="16"
+dataset="cifar10"
+run
+
+arch="vgg16_bn"
+depth="16"
+dataset="cifar100"
+run
