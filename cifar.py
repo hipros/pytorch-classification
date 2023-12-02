9caff1f59ee0373d0e5ba86f5edf35c70188be67
hipros <npclinic3@gmail.com>
2023-12-02 15:40:37 +0900

    change the style of argument name

--- cifar.py
+++ cifar.py
@@ -79,8 +79,8 @@ parser.add_argument('--gpu-id', default='0', type=str,
                     help='id(s) for CUDA_VISIBLE_DEVICES')
 
 # Options
-parser.add_argument('--normalization_transform', default="True", type=str, help="True, False")
-parser.add_argument('--pre_layer', default="None", type=str, help="None, bn, affine, beta, gamma")
+parser.add_argument('--normalization-transform', default="True", type=str, help="True, False")
+parser.add_argument('--pre-layer', default="None", type=str, help="None, bn, affine, beta, gamma")
 
 args = parser.parse_args()
 state = {k: v for k, v in args._get_kwargs()}
@@ -359,3 +359,11 @@ def adjust_learning_rate(optimizer, epoch):
 
 if __name__ == '__main__':
     main()
+
+"""
+parser.add_argument('--no_wd_pre_layer', default="True", type=str, help="True, False")
+args.no_wd_prelayer = eval(args.no_wd_prelayer)
+    parameters_for_training = [param for name, param in model.named_parameters() 
+                      if not (args.no_wd_prelayer and name.startswith('module.pre'))]
+    optimizer = optim.SGD(parameters_for_training, lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+"""
